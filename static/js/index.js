71025f7689627571c1da3e8a944cb856b0c05ce9
王永杰 <admin@xiaodongxier.com>
2021-03-09 13:59:05 +0800

    隐藏留言插件广告语

--- static/js/index.js
+++ static/js/index.js
@@ -3,4 +3,4 @@ setTimeout(function () {
     var y = (mydom.contentWindow || mydom.contentDocument);
     if (y.document) y = y.document;
     y.getElementsByClassName("hi-youke")[0].style.display = "none";
-}, 2500)
+}, 4500)
