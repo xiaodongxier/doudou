5d621c672031fa24ef8d1bad0f1c6052de671a54
小东西儿 <xiaodongxier@gmail.com>
2021-03-16 16:58:17 +0800

    修复已知问题

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
-setTimeout(function(){
+setInterval(function(){
     var mydom = document.querySelector('.youke-chat-window-frame iframe');
     var y = (mydom.contentWindow || mydom.contentDocument);
     if (y.document) y = y.document;
     y.querySelectorAll(".message-list-wrapper .message-list-operations + div")[0].style.display = "none";
-  },2500)
+  },2000)
